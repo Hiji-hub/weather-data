d8bbb23569a54533e1d431f724102fce483daf87
Hiji-hub <71702310+Hiji-hub@users.noreply.github.com>
2021-05-25 17:03:49 +0400

    Add class

--- src/Components/Pages/StartPage.js
+++ src/Components/Pages/StartPage.js
@@ -4,7 +4,7 @@ function StartPage() {
     return(
         <div className='container'>
             <div className='content-time'>
-                <h1 style={{textAlign: 'center', fontSize: '40px'}}>
+                <h1 className='start-title' style={{textAlign: 'center', fontSize: '40px'}}>
                     Добро пожаловать на наш сайт!<br/>
                     Здесь вы можете узнать точное время и проверить погоду в Вашем городе!
                 </h1>
