ed7a0bb36bb5fbb6823394ef833214b40b3206cf
Hiji-hub <71702310+Hiji-hub@users.noreply.github.com>
2021-05-25 14:17:48 +0400

    Изменение роутинга
    Настройка правильного роутинга

--- src/Components/routes.js
+++ src/Components/routes.js
@@ -7,16 +7,16 @@ import Weather from './Pages/Weather'
 export const useRoutes = () => {
     return (
         <Switch>
-            <Route path="/" exact>
+            <Route path="/weather-data" exact>
                 <StartPage/>
             </Route>
-            <Route path="/time" exact>
+            <Route path="/weather-data/time" exact>
                 <Time/>
             </Route>
-            <Route path="/weather" exact>
+            <Route path="/weather-data/weather" exact>
                 <Weather/>
             </Route>
-            <Redirect to="/"/>
+            <Redirect to="/weather-data"/>
         </Switch>
     )
 }
